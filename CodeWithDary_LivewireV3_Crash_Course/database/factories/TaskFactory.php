52f29ec5abf6f7338f2954a47d8ce77c47690266
Railton98 <railtonleal98@gmail.com>
2024-03-21 00:31:26 -0300

    CodeWithDary | Livewire v3 Crash Course: lesson 01 - introduction & setup of livewire

---
+++ CodeWithDary_LivewireV3_Crash_Course/database/factories/TaskFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->sentence(),
+            'user_id' => User::factory(),
+        ];
+    }
+}
---
+++ CodeWithDary_LivewireV3_Crash_Course/database/seeders/TaskSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TaskSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Task::factory()
+            ->count(2)
+            ->for(User::query()->first())
+            ->create();
+    }
+}
