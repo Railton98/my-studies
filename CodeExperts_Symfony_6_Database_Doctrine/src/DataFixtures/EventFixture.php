d5f1c8315f1b499f4603cc71cd12a7d0265d8c5d
Railton98 <railtonleal98@gmail.com>
2024-04-05 18:01:52 -0300

    CodeExperts | Symfony 6 e Database com Doctrine: using faker via foundry

--- CodeExperts_Symfony_6_Database_Doctrine/src/DataFixtures/EventFixture.php
+++ CodeExperts_Symfony_6_Database_Doctrine/src/DataFixtures/EventFixture.php
@@ -2,10 +2,7 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Event;
-use DateTime;
-use DateTimeImmutable;
-use DateTimeZone;
+use App\Factory\EventFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -13,19 +10,6 @@ class EventFixture extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $timezone = new DateTimeZone('America/Sao_Paulo');
-
-        $event = (new Event)
-            ->setTitle('Evento 1')
-            ->setDescription('Evento 1')
-            ->setBody('Evento 1')
-            ->setSlug('evento-1')
-            ->setStartDate(new DateTime('2024-04-05 14:00:00', $timezone))
-            ->setEndDate(new DateTime('2024-04-05 18:00:00', $timezone))
-            ->setCreatedAt(new DateTimeImmutable('now', $timezone))
-            ->setUpdatedAt(new DateTimeImmutable('now', $timezone));
-
-        $manager->persist($event);
-        $manager->flush();
+        EventFactory::createMany(20);
     }
 }
