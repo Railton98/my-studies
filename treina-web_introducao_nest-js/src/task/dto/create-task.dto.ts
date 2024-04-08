c737abbeb21740a46864514b51a29077c16c27dd
Railton98 <railtonleal98@gmail.com>
2024-04-08 17:06:25 -0300

    TreinaWeb | Introdução ao NestJS: lesson 07 - validation with class-validator

--- treina-web_introducao_nest-js/src/task/dto/create-task.dto.ts
+++ treina-web_introducao_nest-js/src/task/dto/create-task.dto.ts
@@ -1,4 +1,13 @@
+import { IsString, Length } from 'class-validator';
+
 export class CreateTaskDto {
+  @IsString({ message: 'O campo Título deve ser uma string' })
+  @Length(3, 10, { message: 'O campo Título deve ter entre 3 e 10 caracteres' })
   titulo: string;
+
+  @IsString({ message: 'O campo Descrição deve ser uma string' })
+  @Length(3, 150, {
+    message: 'O campo Descrição deve ter entre 3 e 150 caracteres',
+  })
   descricao: string;
 }
