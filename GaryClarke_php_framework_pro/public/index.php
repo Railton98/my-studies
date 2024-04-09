7af6617995a9b1afaa2fad389a9c34e0782046c6
Railton98 <railtonleal98@gmail.com>
2024-04-09 16:45:13 -0300

    Gary Clarke | PHP Framework PRO: create Request class

--- GaryClarke_php_framework_pro/public/index.php
+++ GaryClarke_php_framework_pro/public/index.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 require_once dirname(__DIR__).'/vendor/autoload.php';
 
-dd('dsdsd');
-
 // request received
+$request = \Tecks\Framework\Http\Request::createFromGlobals();
+dd($request);
 
 // perform some logic
 
