f2b3fe8dcdd9fb23a7629f2e6a2c1d05966a0c6f
Railton98 <railtonleal98@gmail.com>
2024-04-09 17:30:19 -0300

    Gary Clarke | PHP Framework PRO: create Response class

--- GaryClarke_php_framework_pro/public/index.php
+++ GaryClarke_php_framework_pro/public/index.php
@@ -2,13 +2,18 @@
 
 declare(strict_types=1);
 
+use Tecks\Framework\Http\Request;
+use Tecks\Framework\Http\Response;
+
 require_once dirname(__DIR__).'/vendor/autoload.php';
 
 // request received
-$request = \Tecks\Framework\Http\Request::createFromGlobals();
-dd($request);
+$request = Request::createFromGlobals();
 
 // perform some logic
 
 // send response (string of content)
-echo 'Hello World';
+$content = '<h1>Hello World</h1>';
+
+$response = new Response(content: $content, status: 200, headers: []);
+$response->send();
