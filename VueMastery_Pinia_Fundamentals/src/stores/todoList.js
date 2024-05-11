6a49daa3e5f92c459535de2a9da528a37f9500b1
Railton98 <railtonleal98@gmail.com>
2024-05-11 18:56:55 -0300

    Vue Mastery | Pinia Fundamentals: Deleting state from the store

--- VueMastery_Pinia_Fundamentals/src/stores/todoList.js
+++ VueMastery_Pinia_Fundamentals/src/stores/todoList.js
@@ -10,9 +10,7 @@ export const useTodoListStore = defineStore('todoList', {
       this.todoList.push({ item, id: this.id++, completed: false })
     },
     deleteTodo(itemId) {
-      this.todoList = this.todoList.filter((object) => {
-        return object.id !== itemId
-      })
+      this.todoList = this.todoList.filter((object) => object.id !== itemId)
     },
     toggleCompleted(idToFind) {
       const todo = this.todoList.find((obj) => obj.id === idToFind)
