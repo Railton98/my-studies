3c2d2ad34bf7069f1eb12986a92d1652c5465044
Railton98 <railtonleal98@gmail.com>
2024-05-17 20:59:05 -0300

    CodeWithTony | Vue Router 4: create navbar component

--- CodeWithTony_Vue_Router_4/src/views/Movies.vue
+++ CodeWithTony_Vue_Router_4/src/views/Movies.vue
@@ -3,15 +3,27 @@
   import MovieCard from '../components/MovieCard.vue'
 
   const movieList = ref([])
+  const isLoading = ref(true)
 
-  onMounted(async () => {
-    movieList.value = await (await fetch('http://localhost:8000/movies')).json()
+  onMounted(() => {
+    setTimeout(async () => {
+      movieList.value = await (
+        await fetch('http://localhost:8000/movies')
+      ).json()
+      isLoading.value = false
+    }, 500)
   })
 </script>
 
 <template>
-  <h1>Movies</h1>
-  <div class="grid grid-cols-3 gap-4">
+  <h1 class="text-3xl text-center dark:text-white my-3">Movies</h1>
+
+  <div class="mx-auto" v-if="isLoading">
+    <span class="text-2xl font-bold text-indigo-700 dark:text-indigo-400">
+      Is Loading...
+    </span>
+  </div>
+  <div class="grid grid-cols-3 gap-4" v-else>
     <MovieCard v-for="movie in movieList" :key="movie.id" :movie />
   </div>
 </template>
