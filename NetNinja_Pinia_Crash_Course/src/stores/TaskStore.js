f20bca58a28fa8bc42498d08275c9d3109a94825
Railton98 <railtonleal98@gmail.com>
2024-05-21 12:48:56 -0300

    Net Ninja | Pinia Crash Course: getters

--- NetNinja_Pinia_Crash_Course/src/stores/TaskStore.js
+++ NetNinja_Pinia_Crash_Course/src/stores/TaskStore.js
@@ -1,13 +1,29 @@
 import { defineStore } from 'pinia'
-import { reactive } from 'vue'
+import { computed, reactive } from 'vue'
 
 export const useTaskStore = defineStore('taskStore', () => {
+  // state
   const tasks = reactive([
     { id: 1, title: 'buy some milk', isFav: false },
     { id: 2, title: 'play Gloomhaven', isFav: true },
   ])
 
+  // getters
+  const favs = computed(() => tasks.filter((t) => t.isFav))
+  const favCount = computed(() =>
+    tasks.reduce(
+      (previous, current) => (current.isFav ? previous + 1 : previous),
+      0
+    )
+  )
+  const totalCount = computed(() => tasks.length)
+
+  // actions
+
   return {
     tasks,
+    favs,
+    favCount,
+    totalCount,
   }
 })
