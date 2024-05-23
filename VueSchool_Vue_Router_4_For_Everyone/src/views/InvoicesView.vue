ad3f3040e4e940919bdccf945650813cad512c8d
Railton98 <railtonleal98@gmail.com>
2024-05-23 01:04:35 -0300

    Vue School | Vue Router 4 for Everyone: router and the composition api

--- VueSchool_Vue_Router_4_For_Everyone/src/views/InvoicesView.vue
+++ VueSchool_Vue_Router_4_For_Everyone/src/views/InvoicesView.vue
@@ -1,3 +1,13 @@
+<script setup>
+import { onBeforeRouteLeave } from 'vue-router'
+
+onBeforeRouteLeave(() => {
+  const answer = window.confirm('Are you sure you want to leave? Invoices are super awesome!')
+
+  if (!answer) return false
+})
+</script>
+
 <template>
   <div>
     <h1>Invoices</h1>
