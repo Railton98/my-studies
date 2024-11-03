3cfe54c9bb778f504023b02b911ba0a1c0c4b1fa
Railton L Rodrigues <railtonleal98@gmail.com>
2024-11-03 19:25:28 -0300

    Vue School | Laravel Backends for Vue.js 3: Add Some Application-Wide TS Interfaces

--- VueSchool_Laravel_Backends_for_Vuejs_3/composables/useAuth.ts
+++ VueSchool_Laravel_Backends_for_Vuejs_3/composables/useAuth.ts
@@ -1,16 +1,5 @@
+import type { LoginPayload, RegisterPayload, User } from "@/types";
 import axios from "axios";
-
-interface User {
-  id: number;
-  name: string;
-  email: string;
-  email_verified_at?: Date;
-  two_factor_confirmed_at?: Date;
-  two_factor_recovery_codes?: number;
-  two_factor_secret?: string;
-  created_at: Date;
-  updated_at: Date;
-}
 
 const user = ref<User | null>(null);
 
@@ -42,10 +31,6 @@ export const useAuth = () => {
     user.value = await getUser();
   }
 
-  interface LoginPayload {
-    email: string;
-    password: string;
-  }
   async function login(payload: LoginPayload) {
     await axios.post("/login", payload);
 
@@ -58,12 +43,6 @@ export const useAuth = () => {
     useRouter().replace("/login");
   }
 
-  interface RegisterPayload {
-    name: string;
-    email: string;
-    password: string;
-    password_confirmation: string;
-  }
   async function register(payload: RegisterPayload) {
     await axios.post("/register", payload);
 
