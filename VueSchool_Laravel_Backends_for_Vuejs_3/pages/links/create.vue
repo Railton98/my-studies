5a91071ec270eb59d5ab1f89af8aaee48e74528a
Railton L Rodrigues <railtonleal98@gmail.com>
2024-11-03 19:08:04 -0300

    Vue School | Laravel Backends for Vue.js 3: Create New Resources with Laravel and Vue

--- VueSchool_Laravel_Backends_for_Vuejs_3/pages/links/create.vue
+++ VueSchool_Laravel_Backends_for_Vuejs_3/pages/links/create.vue
@@ -1,17 +1,31 @@
 <script setup lang="ts">
+import type { FormKitNode } from "@formkit/core";
+import axios from "axios";
+import { nanoid } from "nanoid";
+
 definePageMeta({
   middleware: ["auth"],
 });
+
+async function createLink(payload, node?: FormKitNode) {
+  try {
+    await axios.post("/links", {
+      ...payload,
+      short_link: nanoid(8),
+    });
+
+    useRouter().push("/links");
+  } catch (err) {
+    handleInvalidForm(err, node);
+  }
+}
 </script>
 
 <template>
   <h1>Create New Link</h1>
   <GoBack>or go back to links</GoBack>
-  <form>
-    <label>
-      <div>Link</div>
-      <input type="text" name="full_link" />
-    </label>
-    <button class="btn">Create Link</button>
-  </form>
+
+  <FormKit type="form" submit-label="Create Link" @submit="createLink">
+    <FormKit label="Link" type="url" name="full_link" />
+  </FormKit>
 </template>
