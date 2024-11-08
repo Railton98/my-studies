cf6642763ce2a08c8978d8dc3dc750ef02ce3731
Railton L Rodrigues <railtonleal98@gmail.com>
2024-11-08 17:15:17 -0300

    Pinguim.Academy [Tudo o que testo] - testing http calls

---
+++ tudo-o-que-testo/app/Console/Commands/ExportProductToAmazonCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Product;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+
+class ExportProductToAmazonCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:export-product-to-amazon-command';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        Http::withToken(config('services.amazon.api_key'))->post(
+            'https://api.amazon.com/products',
+            Product::all()->map(fn ($p) => ['title' => $p->title])->toArray()
+        );
+    }
+}
--- tudo-o-que-testo/tests/Feature/HttpCallsTest.php
+++ tudo-o-que-testo/tests/Feature/HttpCallsTest.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Console\Commands\ExportProductToAmazonCommand;
 use App\Console\Commands\ImportFromAmazonCommand;
 use App\Models\Product;
 use App\Models\User;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Http\Client\Request;
 
 use function Pest\Laravel\artisan;
 use function Pest\Laravel\assertDatabaseCount;
@@ -11,7 +13,7 @@ use function Pest\Laravel\assertDatabaseHas;
 
 it('should fake an api request', function () {
     User::factory()->create();
-    
+
     Http::fake([
         'https://api.amazon.com/products' => Http::response([
             ['title' => 'Product 1'],
@@ -25,4 +27,26 @@ it('should fake an api request', function () {
     assertDatabaseCount(Product::class, 2);
     assertDatabaseHas(Product::class, ['title' => 'Product 1']);
     assertDatabaseHas(Product::class, ['title' => 'Product 2']);
+});
+
+test('testing the data that we send to amazon', function () {
+    Http::fake();
+    config()->set('services.amazon.api_key', 1234314324523);
+
+    Product::factory()->count(2)->create();
+
+    (new ExportProductToAmazonCommand)->handle();
+
+    Http::assertSent(function (Request $request) {
+        return $request->url() === 'https://api.amazon.com/products'
+            && $request->header('Authorization') === ['Bearer '.config('services.amazon.api_key')]
+            && $request->data() == Product::all()->map(fn ($p) => ['title' => $p->title])->toArray();
+    });
+});
+
+it('my config should have at least the key', function () {
+    expect(config('services'))
+        ->toHaveKey('amazon')
+        ->and(config('services.amazon'))
+        ->toHaveKey('api_key');
 });
