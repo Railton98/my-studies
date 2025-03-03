fee13db86c9fc5e1620ce63707f563389f15cd1f
Railton L Rodrigues <railtonleal98@gmail.com>
2025-03-03 20:03:11 -0300

    Vue School | Real-Time Data with Laravel Reverb and Vue.js: Listening to Laravel Reverb Events in Vue

---
+++ VueSchool_Laravel_Reverb_Vuejs/resources/js/Pages/VisitCount.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from "vue";
+
+const count = ref(0);
+Echo.channel("hello-world").listen("HelloWorld", (e) => {
+    count.value++;
+});
+</script>
+
+<template>
+    <div>Visit Count is {{ count }}</div>
+</template>
