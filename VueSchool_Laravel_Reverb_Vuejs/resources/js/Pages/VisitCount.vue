e73d64ee732618b064803eccdfa2f5c06f787ece
Railton L Rodrigues <railtonleal98@gmail.com>
2025-03-03 20:32:09 -0300

    Vue School | Real-Time Data with Laravel Reverb and Vue.js: Send Messages in Real Time With Vue

--- VueSchool_Laravel_Reverb_Vuejs/resources/js/Pages/VisitCount.vue
+++ VueSchool_Laravel_Reverb_Vuejs/resources/js/Pages/VisitCount.vue
@@ -1,12 +1,42 @@
 <script setup>
 import { ref } from "vue";
 
-const count = ref(0);
-Echo.channel("hello-world").listen("HelloWorld", (e) => {
-    count.value++;
+const messages = ref([]);
+const message = ref("");
+
+Echo.channel("messages").listen("MessageReceived", (e) => {
+    if (!messages.value.find((m) => m.id === e.id)) {
+        messages.value.push({
+            ...e,
+            who: "Them",
+        });
+    }
 });
+
+function handleSubmit() {
+    const msg = {
+        id: crypto.randomUUID(),
+        message: message.value,
+    };
+    messages.value.push({
+        ...msg,
+        who: "Me",
+    });
+    axios.post("/messages", msg);
+
+    message.value = "";
+}
 </script>
 
 <template>
-    <div>Visit Count is {{ count }}</div>
+    <ul class="max-w-md space-y-1 list-none list-inside">
+        <li v-for="message in messages" :key="message.id">
+            {{ message.message }} - {{ message.who }}
+        </li>
+    </ul>
+
+    <form @submit.prevent="handleSubmit">
+        <textarea v-model="message"></textarea>
+        <button>Send Message</button>
+    </form>
 </template>
