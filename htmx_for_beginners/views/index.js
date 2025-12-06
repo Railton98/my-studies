eaa70e542115402278269b5341701af83a18e3f3
Railton L Rodrigues <railtonleal98@gmail.com>
2025-12-06 17:58:12 -0300

    HTMX  for Beginners: lesson 3 - Get Requests (hx-get)

--- htmx_for_beginners/views/index.js
+++ htmx_for_beginners/views/index.js
@@ -11,7 +11,7 @@ const createHomePageTemplate = () => /*html*/ `
         </header>
         <main>
             <div class="book-list">
-                <!-- book list here later -->
+                <button hx-get="/books">Show Books</button>
             </div>
 
             <div class="add-book-form">
