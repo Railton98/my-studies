ce1c20f31912d907382760715d84efdf0fb83104
Railton L Rodrigues <railtonleal98@gmail.com>
2026-01-05 19:11:36 -0300

    HTMX for Beginners: lesson 9 - Default & Custom Triggers

--- htmx_for_beginners/views/index.js
+++ htmx_for_beginners/views/index.js
@@ -11,23 +11,20 @@ const createHomePageTemplate = () => /*html*/ `
         </header>
         <main>
             <div class="book-list">
-                <button hx-get="/books" hx-target=".book-list">Show Books</button>
+                <button hx-get="/books" hx-target=".book-list" hx-trigger="dblclick">Show Books</button>
             </div>
 
             <div class="add-book-form">
                 <h2>What do you want to read?</h2>
-                <form>
-                    <input type="text" name="title" placeholder="Title"/>
-                    <input type="text" name="author" placeholder="Author"/>
-                    <button
-                        hx-on:click="console.log('new book added', event)"
-                        hx-on::after-request="document.querySelector('form').reset()"
-                        hx-post="books"
-                        hx-target=".book-list ul"
-                        hx-swap="beforeend"
-                    >
-                        Add Book
-                    </button>
+                <form
+                    hx-post="/books"
+                    hx-target=".book-list ul"
+                    hx-swap="beforeend"
+                    hx-on::after-request="document.querySelector('form').reset()"
+                >
+                    <input type="text" name="title" placeholder="Title" required />
+                    <input type="text" name="author" placeholder="Author" required />
+                    <button>Add Book</button>
                 </form>
             </div>
         </main>
